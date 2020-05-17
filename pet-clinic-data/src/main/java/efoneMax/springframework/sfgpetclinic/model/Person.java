5d1f88bba734429ad4f0079fd1447462e9abf286
glushkov <glushkov@proitr.ru>
2020-05-17 19:41:21 +0500

    adding base entity. Closes #10

--- pet-clinic-data/src/main/java/efoneMax/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/efoneMax/springframework/sfgpetclinic/model/Person.java
@@ -3,7 +3,7 @@ package efoneMax.springframework.sfgpetclinic.model;
 import lombok.Data;
 
 @Data
-public class Person {
+public class Person extends BaseEntity {
     private String firstname;
     private String lastname;
 }
