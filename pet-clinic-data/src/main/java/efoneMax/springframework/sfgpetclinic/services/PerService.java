2b3c46d94ab8b94e2a960babf420b312f9003d30
glushkov <glushkov@proitr.ru>
2020-05-24 18:05:22 +0500

    refactoring to common interface. Closes #11

--- pet-clinic-data/src/main/java/efoneMax/springframework/sfgpetclinic/services/PerService.java
+++ pet-clinic-data/src/main/java/efoneMax/springframework/sfgpetclinic/services/PerService.java
@@ -2,12 +2,5 @@ package efoneMax.springframework.sfgpetclinic.services;
 
 import efoneMax.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PerService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PerService extends CrudService<Pet, Long> {
 }
