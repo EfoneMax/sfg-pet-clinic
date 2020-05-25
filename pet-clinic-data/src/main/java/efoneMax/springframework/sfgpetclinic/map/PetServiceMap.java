cdb0fcd2206bcc0393265d64ac2a772dd10aa038
glushkov <glushkov@proitr.ru>
2020-05-25 13:27:43 +0500

    Impl Map bases services, closes #6

---
+++ pet-clinic-data/src/main/java/efoneMax/springframework/sfgpetclinic/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package efoneMax.springframework.sfgpetclinic.map;
+
+import efoneMax.springframework.sfgpetclinic.model.Pet;
+import efoneMax.springframework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public void delete(Pet Pet) {
+        super.delete(Pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
