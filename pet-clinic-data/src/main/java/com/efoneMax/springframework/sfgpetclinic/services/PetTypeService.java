8e45f248e153456549be127247fea0ea67e6cfa0
glushkov <glushkov@proitr.ru>
2020-06-08 12:18:24 +0500

    Created Pet Type Map Service. Closes #29

---
+++ pet-clinic-data/src/main/java/com/efoneMax/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.efonemax.springframework.sfgpetclinic.services;
+
+import com.efonemax.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
