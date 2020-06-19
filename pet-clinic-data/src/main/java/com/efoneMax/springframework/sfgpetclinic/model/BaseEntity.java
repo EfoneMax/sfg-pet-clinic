c96c2d5e560bae6cf9391c4cf6f3acd9df4cb9b1
glushkov <glushkov@proitr.ru>
2020-06-19 12:11:20 +0500

    Updated dase entity with JPA annotations. Closes #35

--- pet-clinic-data/src/main/java/com/efoneMax/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/efoneMax/springframework/sfgpetclinic/model/BaseEntity.java
@@ -3,11 +3,18 @@ package com.efonemax.springframework.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity implements Serializable {
-    private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
 }
