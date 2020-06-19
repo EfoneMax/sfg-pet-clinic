564419391aba98532325bb199ed59704576f6fdf
glushkov <glushkov@proitr.ru>
2020-06-19 13:01:40 +0500

    Added Visit and Pet Entity. Closes #38

--- pet-clinic-data/src/main/java/com/efoneMax/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/efoneMax/springframework/sfgpetclinic/model/Pet.java
@@ -3,17 +3,21 @@ package com.efonemax.springframework.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
 @Entity
-@Table
+@Table(name = "pets")
 public class Pet extends BaseEntity {
     @Column(name = "name")
     private String name;
@@ -26,6 +30,10 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
 
-    @Column(name = "bith_date")
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    Set<Visit> visits = new HashSet<>();
+
 }
--- pet-clinic-data/src/main/java/com/efoneMax/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/efoneMax/springframework/sfgpetclinic/model/Visit.java
@@ -1,9 +1,22 @@
 package com.efonemax.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
+    @Column(name = "local_date")
     private LocalDate localDate;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 }
