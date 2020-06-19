58675e37a6c54301e0dae80f4f219e30fe91bb3a
glushkov <glushkov@proitr.ru>
2020-06-19 12:36:14 +0500

    Updated owner with JPA entity mappings. Closes #36

--- pet-clinic-data/src/main/java/com/efoneMax/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/efoneMax/springframework/sfgpetclinic/model/Owner.java
@@ -3,14 +3,29 @@ package com.efonemax.springframework.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owners")
     private Set<Pet> pets = new HashSet<>();
 }
--- pet-clinic-data/src/main/java/com/efoneMax/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/efoneMax/springframework/sfgpetclinic/model/Person.java
@@ -1,9 +1,18 @@
 package com.efonemax.springframework.sfgpetclinic.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@Setter
+@MappedSuperclass
 public class Person extends BaseEntity {
+    @Column(name = "first_name")
     private String firstname;
+    @Column(name = "last_name")
     private String lastname;
 }
