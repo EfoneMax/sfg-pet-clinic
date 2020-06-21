c36bfdedc952d16f3e95d438901543cf4929d77c
glushkov <glushkov@proitr.ru>
2020-06-21 18:31:38 +0500

    Created Visit Map service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/efoneMax/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.efonemax.springframework.sfgpetclinic.services;
+
+import com.efonemax.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
