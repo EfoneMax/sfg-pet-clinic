b1dba2474c905cf504fc8c60ea95f6fec2e51d9f
glushkov <glushkov@proitr.ru>
2020-07-20 22:50:08 +0500

    Closes #55 Implement Find Owner functionality

--- pet-clinic-web/src/main/java/com/efonemax/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/efonemax/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -39,12 +39,6 @@ public class OwnerController {
         return "owners/findOwners";
     }
 
-    @GetMapping({"", "/","/index", "/index.html"})
-    public String listOwners(Model model) {
-        model.addAttribute("owners", ownerService.findAll());
-        return "owners/index";
-    }
-
     @GetMapping("/{ownerId}")
     public ModelAndView showOwner(@PathVariable Long ownerId) {
         ModelAndView mav = new ModelAndView("owners/ownerDetails");
@@ -72,6 +66,31 @@ public class OwnerController {
     public String initUpdateOwnerForm(@PathVariable Long ownerId, Model model) {
         model.addAttribute(ownerService.findById(ownerId));
         return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+    }
+
+    @GetMapping
+    public String processFindForm(Owner owner, BindingResult result, Model model){
+        // allow parameterless GET request for /owners to return all records
+        if (owner.getLastName() == null) {
+            owner.setLastName(""); // empty string signifies broadest possible search
+        }
+
+        // find owners by last name
+        List<Owner> results = ownerService.findAllByLastNameLike("%"+ owner.getLastName() + "%");
+
+        if (results.isEmpty()) {
+            // no owners found
+            result.rejectValue("lastName", "notFound", "not found");
+            return "owners/findOwners";
+        } else if (results.size() == 1) {
+            // 1 owner found
+            owner = results.get(0);
+            return "redirect:/owners/" + owner.getId();
+        } else {
+            // multiple owners found
+            model.addAttribute("selections", results);
+            return "owners/ownersList";
+        }
     }
 
     @PostMapping("/{ownerId}/edit")
